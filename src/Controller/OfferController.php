57f05ecb1c0be31fe532c4a30824d07633b94e86
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-05-31 16:00:28 +0200

    show offer

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -2,20 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Offer;
 use App\Repository\OfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/offer", name="offer_")
+ */
 class OfferController extends AbstractController
 {
     /**
-     * @Route("/offer", name="offer")
+     * @Route("/", name="list")
      */
     public function index(OfferRepository $offerRepository): Response
     {
         return $this->render('offer/index.html.twig', [
             'offers' => $offerRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show")
+     */
+    public function show(Offer $offer): Response
+    {
+        return $this->render('offer/show.html.twig', [
+            'offer' => $offer,
         ]);
     }
 }
