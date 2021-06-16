a7d7876c6a4cc839011bbef024c4f51b1f78b0fa
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-06-16 11:09:44 +0200

    user

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/offer", name="offer_")
@@ -28,6 +29,7 @@ class OfferController extends AbstractController
 
     /**
      * @Route("/add", name="add")
+     * @IsGranted("ROLE_USER")
      */
     public function add(Request $request, EntityManagerInterface $entityManager): Response
     {
